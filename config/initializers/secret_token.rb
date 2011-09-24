c1aae51cf571a64aba79f23e9af22996914fca5d
Alexey Poimtsev <alec@alec-c4.com>
2011-09-24 14:19:43 +0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C4cDeklarator::Application.config.secret_token = '507b8e77ce0f0a7650dc74527582fb63a17da395c1121047d32e4337a551018537368e1e204ba81c3298115a20afc68d0c1a769b086e7f67c27c433e0b1f7222'
